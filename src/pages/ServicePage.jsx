171ff218e7f64ac6af85e2cfa6abb6e7f614511f
lxxsh <happy750633@gmail.com>
2026-05-12 15:27:50 +0900

    Remove page scaling wrapper

--- src/pages/ServicePage.jsx
+++ src/pages/ServicePage.jsx
@@ -1,43 +1,11 @@
-import { useEffect, useState } from "react";
 import ServiceTopbar from "../components/ServiceTopbar";
 import Home from "./Home";
 
-const DESIGN_WIDTH = 1565;
-const DESIGN_HEIGHT = 1070;
-
-function getServiceScale() {
-  if (typeof window === "undefined") return 1;
-
-  return Math.min(
-    window.innerWidth / DESIGN_WIDTH,
-    window.innerHeight / DESIGN_HEIGHT,
-    1
-  );
-}
-
 function ServicePage() {
-  const [scale, setScale] = useState(getServiceScale);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setScale(getServiceScale());
-    };
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
-    <div
-      className="service-scale-viewport"
-      style={{ "--service-scale": scale }}
-    >
-      <div className="service-scale-canvas">
-        <ServiceTopbar />
-        <Home />
-      </div>
+    <div>
+      <ServiceTopbar />
+      <Home />
     </div>
   );
 }
